ca212fc8d6dd2306c34c399ef38c6a6cc2a172cb
SaraW011 <87926715+SaraW011@users.noreply.github.com>
2022-04-29 19:41:40 +0300

    update api file name

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import EditAvatarPopup from "./EditAvatarPopup";
 import AddPlacePopup from "./AddPlacePopup";
 import ConfirmDeletePopup from "./ConfirmDeletePopup";
 
-import api from "../utils/Api";
+import api from "../utils/api";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function App() {
